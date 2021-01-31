df7d55322ea1797b8890f556f11cc20e69fed768
fanshi1028 <jackychany321@gmail.com>
2021-01-31 13:00:36 +0800

    try flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Servant with beam";
+
+  # inputs.nixpkgs.url = "nixpkgs";
+
+  # inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.url = "github:input-output-hk/haskell.nix";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, # haskellNix,
+    nixpkgs, flake-utils }:
+
+    # flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    let
+      # nixpkgs = haskellNix.legacyPackages.${system};
+      project-args = {
+        src = nixpkgs.nix-gitignore.gitignoreSource [ ] ./.;
+        compiler-nix-name = "ghc8102";
+      };
+      overlay = self: super: {
+        haskell-nix = super.haskell-nix // {
+          toolPackageName = super.haskell-nix.toolPackageName // {
+            gen-hie = "implicit-hie";
+          };
+          packageToolName = super.haskell-nix.packageToolName // {
+            implicit-hie = "gen-hie";
+          };
+        };
+        servant-with-beam = self.haskell-nix.project project-args;
+      };
+    in flake-utils.lib.simpleFlake {
+      # defaultPackage =  project-args;
+
+      name = "servant-with-beam";
+
+      inherit self nixpkgs overlay;
+
+      # shell = ./shell.nix;
+
+      # systems = [ "x86_64-linux" "x86_64-darwin" ];
+      systems = [ "x86_64-darwin" ];
+    };
+  # );
+}
