f7255c12ccd6dde0d5320212d802d1cba8402811
fanshi1028 <jackychany321@gmail.com>
2021-02-03 22:07:09 +0800

    fix source cleansing

--- default.nix
+++ default.nix
@@ -1,15 +1,28 @@
 { pkgs ? import ./nix/pkgs.nix { } }:
-pkgs.haskell-nix.project {
+let
+  hLib = pkgs.haskell-nix.haskellLib;
+
+  includedFiles = [ "app" "src" "tests" ];
+
+  name = "servant-with-beam";
+  baseSrc = ./.;
   # 'cleanGit' cleans a source directory based on the files known by git
   # NOTE https://github.com/input-output-hk/haskell.nix/issues/492
   # NOTE cleanGit not lorri friendly
-  # src = pkgs.haskell-nix.haskellLib.cleanGit {
-  #   name = "servantWithBeam";
-  #   src = ./.;
-  # };
   # NOTE use less good nix-gitignore instead for now
-  src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-
+  # src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+  # NOTE https://github.com/input-output-hk/haskell.nix/issues/1013
+  src = hLib.cleanSourceWith {
+    inherit name;
+    filter = path: type:
+      pkgs.lib.any (f:
+        let p = toString (baseSrc + ("/" + f));
+        in p == path || (pkgs.lib.strings.hasPrefix (p + "/") path))
+      includedFiles || baseNameOf path == "${name}.cabal";
+    src = baseSrc;
+  };
+in pkgs.haskell-nix.project {
+  inherit src;
   # For `cabal.project` based projects specify the GHC version to use.
   compiler-nix-name = "ghc8102"; # Not used for `stack.yaml` based projects.
   # modules = [{
