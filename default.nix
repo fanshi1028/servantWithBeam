cc5154776a0a8de76c02b51d593f9f6ad962f87b
fanshi1028 <jackychany321@gmail.com>
2021-02-13 17:16:30 +0800

    fix github action

--- default.nix
+++ default.nix
@@ -25,11 +25,11 @@ let
   compiler-nix-name = compiler;
 in pkgs.haskell-nix.project {
   inherit src compiler-nix-name;
-  cabal-install = pkgs.haskell-nix.hackage-package {
-    inherit compiler-nix-name;
-    name = "cabal-install";
-    version = "3.2.0.0";
-  };
+  # cabal-install = pkgs.haskell-nix.hackage-package {
+  #   inherit compiler-nix-name;
+  #   name = "cabal-install";
+  #   version = "3.2.0.0";
+  # };
   modules = [{
     # NOTE https://github.com/input-output-hk/haskell.nix/issues/720#issuecomment-745397468
     reinstallableLibGhc = true;
