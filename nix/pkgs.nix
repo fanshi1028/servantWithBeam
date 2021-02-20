2ebc11d0d808482e59728d49229e7c145f433b7b
fanshi1028 <jackychany321@gmail.com>
2021-02-20 14:29:51 +0800

    use nixpkgs unstable instead of 2009

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,7 +1,8 @@
 { compiler ? "ghc8102", sources ? import ./sources.nix }:
 let
   haskellNix = import sources.haskell-nix { };
-  nixpkgsSrc = haskellNix.sources.nixpkgs-2009;
+  # nixpkgsSrc = haskellNix.sources.nixpkgs-2009;
+  nixpkgsSrc = haskellNix.sources.nixpkgs-unstable;
   nixpkgsArgs = haskellNix.nixpkgsArgs;
   overlays = haskellNix.overlays ++ [
     (_: super: {
