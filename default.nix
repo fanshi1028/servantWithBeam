7c1101106a3de4cbab11c880837158a81fee6ba0
fanshi1028 <jackychany321@gmail.com>
2021-03-26 09:53:27 +0800

    [mobile] refeactor nix

--- default.nix
+++ default.nix
@@ -40,7 +40,8 @@ let
       # NOTE https://github.com/input-output-hk/haskell.nix/issues/1013
       ignore = path:
         baseNameOf path != ".envrc" || baseNameOf path != "shell.nix"
-        || baseNameOf path != ".ghcid" || baseNameOf path != "servant-with-beam-frontend.cabal";
+        || baseNameOf path != ".ghcid" || baseNameOf path
+        != "servant-with-beam-frontend.cabal";
       filter = path: type:
         ignore path && any (f:
           let p = toString (baseSrc + ("/" + f));
@@ -124,52 +125,37 @@ let
       index-state = "2021-03-19T00:00:00Z";
     };
   def = mkProject pkgs "0000000000000000000000000000000000000000000000000000";
-  rp = (reflexPlatform {
+  rp = reflexPlatform {
     config = {
       android_sdk.accept_license = true;
       allowBroken = true;
     };
 
-    # haskellOverlaysPost = [
-    #   (self: super: {
-    #     servant-with-beam =
-    #       pkgs.haskellPackages.callCabal2nixWithOptions "servant-with-beam"
-    #       ./. "-ffrontend" { };
-    #     # reflex-dom in reflex-platform was created by callCabal2nix which seems to be not respecting os conditional in its cabal, and cause dependency issue
-    #     # https://github.com/reflex-frp/reflex-platform/blob/f019863c21ee85498e6a6e0072e617b2462b70ed/haskell-overlays/reflex-packages/default.nix#L83
-    #     reflex-dom = pkgs.haskellPackages.lib.overrideCabal super.reflex-dom
-    #       (drv: {
-    #         # Hack until https://github.com/NixOS/cabal2nix/pull/432 lands
-    #         libraryHaskellDepends = (drv.libraryHaskellDepends or [ ])
-    #           ++ pkgs.lib.optionals pkgs.hostPlatform.isAndroid [
-    #             self.android-activity
-    #             self.aeson
-    #             self.data-default
-    #             self.jsaddle
-    #           ] ++ pkgs.lib.optionals pkgs.hostPlatform.isIos [
-    #             self.data-default
-    #             self.jsaddle
-    #             self.jsaddle-wkwebview
-    #           ];
-    #       });
-    #   })
-    # ] ;
-  }).project ({ pkgs, ... }: {
-    packages = { servant-with-beam-frontend = ./frontend; };
-    android.servant-with-beam-frontend = {
-      executableName = "frontend";
-      applicationId = "my.frontend";
-      displayName = "Android App";
-    };
-    ios.servant-with-beam-frontend = {
-      executableName = "frontend";
-      bundleIdentifier = "my.frontend";
-      bundleName = "IOS App";
-    };
-    overrides = self: super: {
-      universum = pkgs.haskell.lib.dontCheck super.universum;
-    };
-  });
+    haskellOverlaysPost = [
+      (self: super: {
+        servant-with-beam-frontend =
+          self.callCabal2nix "servant-with-beam-frontend" ./frontend { };
+        # reflex-dom in reflex-platform was created by callCabal2nix which seems to be not respecting os conditional in its cabal, and cause dependency issue
+        # https://github.com/reflex-frp/reflex-platform/blob/f019863c21ee85498e6a6e0072e617b2462b70ed/haskell-overlays/reflex-packages/default.nix#L83
+      })
+    ];
+  };
+  #   .project ({ pkgs, ... }: {
+  #   packages = { servant-with-beam-frontend = ./frontend; };
+  #   android.servant-with-beam-frontend = {
+  #     executableName = "frontend";
+  #     applicationId = "my.frontend";
+  #     displayName = "Android App";
+  #   };
+  #   ios.servant-with-beam-frontend = {
+  #     executableName = "frontend";
+  #     bundleIdentifier = "my.frontend";
+  #     bundleName = "IOS App";
+  #   };
+  #   overrides = self: super: {
+  #     universum = pkgs.haskell.lib.dontCheck super.universum;
+  #   };
+  # });
   releases = {
     linux = def;
     osx = def;
@@ -191,24 +177,25 @@ let
 in if (default) then
   exes.${platform}
 else {
-  servant-with-beam = exes // {
-    android = rp.android.servant-with-beam-frontend;
-    ios = rp.ios.servant-with-beam-frontend;
-  };
-  #                     // {
-  #   android = (rp aarch64-android-prebuilt).android.buildApp ({
-  #     package = p: p.servant-with-beam;
-  #     executableName = "frontend";
-  #     applicationId = "my.frontend";
-  #     displayName = "Android App";
-  #   });
-  #   ios = (rp iphone64).ios.buildApp ({
-  #     package = p: p.servant-with-beam;
-  #     executableName = "frontend";
-  #     bundleIdentifier = "my.frontend";
-  #     bundleName = "IOS App";
-  #   });
-  # };
+  servant-with-beam = exes
+    #                     // {
+    #   android = rp.android.servant-with-beam-frontend;
+    #   ios = rp.ios.servant-with-beam-frontend;
+    # };
+    // {
+      android = rp.android.buildApp ({
+        package = p: p.servant-with-beam-frontend;
+        executableName = "frontend";
+        applicationId = "my.frontend";
+        displayName = "Android App";
+      });
+      ios = rp.ios.buildApp ({
+        package = p: p.servant-with-beam-frontend;
+        executableName = "frontend";
+        bundleIdentifier = "my.frontend";
+        bundleName = "IOS App";
+      });
+    };
   pkgSet = pkgs;
   inherit shells;
 }
